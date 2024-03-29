e6b96e50f5b482fd4eeb4b985268f447b7c9e8cd
Saad Ali Khan <saadalikhan042@gmail.com>
2024-03-29 16:25:40 +0500

    Top Selling component complete

---
+++ src/components/topSelling/TopSelling.jsx
@@ -0,0 +1,62 @@
+import React from "react";
+import tshirt from "../../assets/tshirt.png";
+
+const TopSelling = () => {
+  return (
+    <div className="mt-16 ">
+      <h1 className="text-center font-aclonica font-extrabold text-[2rem]">
+        TOP SELLING
+      </h1>
+      <div className="flex items-center   mt-16 flex-wrap justify-between m-auto max-w-[90%]">
+        <div className="flex max-sm:w-[30rem] flex-col justify-center">
+          <div className="rounded-xl flex items-center justify-center bg-[#f0eeed]">
+            <img src={tshirt} alt="" />
+          </div>
+          <div>
+            <p className="text-gray-500">T-shirt with Tape Details</p>
+            <p className="text-gray-500">Rating 4.5/5</p>
+            <p className="text-gray-500 text-lg font-bold">$120</p>
+          </div>
+        </div>
+        <div className="flex max-sm:w-[30rem] flex-col justify-center">
+          <div className="rounded-xl flex items-center justify-center bg-[#f0eeed]">
+            <img src={tshirt} alt="" />
+          </div>
+          <div>
+            <p className="text-gray-500">T-shirt with Tape Details</p>
+            <p className="text-gray-500">Rating 4.5/5</p>
+            <p className="text-gray-500 text-lg font-bold">$120</p>
+          </div>
+        </div>
+        <div className="flex max-sm:w-[30rem] flex-col justify-center">
+          <div className="rounded-xl flex items-center justify-center bg-[#f0eeed]">
+            <img src={tshirt} alt="" />
+          </div>
+          <div>
+            <p className="text-gray-500">T-shirt with Tape Details</p>
+            <p className="text-gray-500">Rating 4.5/5</p>
+            <p className="text-gray-500 text-lg font-bold">$120</p>
+          </div>
+        </div>
+        <div className="flex max-sm:w-[30rem] flex-col justify-center">
+          <div className="rounded-xl flex items-center justify-center bg-[#f0eeed]">
+            <img src={tshirt} alt="" />
+          </div>
+          <div>
+            <p className="text-gray-500">T-shirt with Tape Details</p>
+            <p className="text-gray-500">Rating 4.5/5</p>
+            <p className="text-gray-500 text-lg font-bold">$120</p>
+          </div>
+        </div>
+      </div>
+
+      <div className="mt-8 flex items-center justify-center">
+        <button className="border font-semibold w-[10rem] p-3 rounded-full">
+          View All
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default TopSelling;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import HeroBanner from "../components/heroBanner/HeroBanner";
 import Companies from "../components/companies/Companies";
 import NewArrivals from "../components/newArrivals/NewArrivals";
+import TopSelling from "../components/topSelling/TopSelling";
 const Home = () => {
   return (
     <div>
       <HeroBanner />
       <Companies />
       <NewArrivals />
+      <TopSelling />
     </div>
   );
 };

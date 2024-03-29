d437026524687e68a1700b679e1e4fce30c31628
Saad Ali Khan <saadalikhan042@gmail.com>
2024-03-29 08:44:53 +0500

    New arrival component complete

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import HeroBanner from "../components/heroBanner/HeroBanner";
 import Companies from "../components/companies/Companies";
+import NewArrivals from "../components/newArrivals/NewArrivals";
 const Home = () => {
   return (
     <div>
       <HeroBanner />
       <Companies />
+      <NewArrivals />
     </div>
   );
 };

1b583ca73d370b434ca1e61fbfa9fea0e018feae
Saad Ali Khan <saadalikhan042@gmail.com>
2024-04-02 15:00:39 +0500

    your cart component complete

--- src/components/yourCart/YourCart.jsx
+++ src/components/yourCart/YourCart.jsx
@@ -5,15 +5,17 @@ import CartShirt from "../../assets/cart_shirt.svg";
 const YourCart = () => {
   return (
     <div className="mt-10 border-t-4 pt-8 m-auto max-w-[90%]">
-      <h1 className="font-extrabold mb-10 text-[2rem]">YOUR CART</h1>
-      <div className="flex justify-between">
-        <div className="flex border-2 justify-between w-[40%] rounded-2xl p-4">
+      <h1 className="font-extrabold text-center mb-10 text-[2rem]">
+        YOUR CART
+      </h1>
+      <div className="flex justify-around flex-wrap max-md:items-center max-md:justify-center ">
+        <div className="flex border-2 justify-between rounded-2xl max-md:mb-4 p-4">
           <div className="flex flex-col justify-between gap-4">
             <div className="flex border-b-2 justify-between pb-4 gap-2">
               <div>
                 <img src={CartShirt} alt="" />
               </div>
-              <div className="flex justify-between gap-96">
+              <div className="flex justify-between">
                 <div className="flex flex-col gap-2">
                   <h4>Gradient Graphic Tshirt</h4>
                   <p>
@@ -87,10 +89,10 @@ const YourCart = () => {
           </div>
         </div>
         <div>
-          <div className="flex gap-4  rounded-2xl border-2 p-4 flex-col justify-between">
+          <div className="flex gap-4  max-sm:p-6 rounded-2xl border-2 p-8 flex-col justify-between">
             <h1>Order Summary</h1>
 
-            <div className="flex  justify-between">
+            <div className="flex max-md:gap-60 max-sm:gap-40 gap-80 justify-between">
               <p>Subtotal</p>
               <p>$565</p>
             </div>
@@ -102,7 +104,7 @@ const YourCart = () => {
               <p>Total</p>
               <p>$580</p>
             </div>
-            <button className="bg-black w-[55%] text-white p-[0.6rem] pl-6 pr-6 rounded-full">
+            <button className="bg-black w-[55%] max-sm:p-2 text-white p-[0.6rem] pl-6 pr-6 rounded-full">
               Go to Checkout
             </button>
           </div>
